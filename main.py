8073b991a4170c7bca2009271bde7797256c3c4f
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2022-03-08 20:29:29 -0800

    brute forced testing to get some few maps on window

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import math
 
+### force brutes to getting some percentage base heat maps less than 5 on the window
+
 #initalizes game 
 class game():
     def __init__(self):
@@ -9,7 +11,7 @@ class game():
         self.WIDTH = 640
         self.HEIGHT = 640
         self.screen = pygame.display.set_mode((self.WIDTH,self.HEIGHT))
-        self.objs = {"AMZN":50,"GOOG":35,"SHOP":15}  
+        self.objs = {"AMZN":50,"GOOG":35,"SHOP":15,"tt":5}  
         self.font = pygame.font.Font("freesansbold.ttf",32)
         
     def algo(self):
@@ -23,44 +25,29 @@ class game():
             self.objPercentage.update({i:percentage})
         #print(self.objPercentage)
         self.x = 0
-        heatsquare = self.area * .5
-        self.L = math.sqrt(heatsquare)
-        self.W = math.sqrt(heatsquare)
-        self.x2 = self.L
-        heatsquare2 = self.area * .35
-        self.L2 = math.sqrt(heatsquare2)
-        self.W2 = math.sqrt(heatsquare2)
-        if (self.L2*self.W2 + self.x2) >= self.WIDTH-10:
-            self.W2 = self.WIDTH - self.x
-            self.L2 = heatsquare2/self.W2
-            print("bigger")
-
-        #self.objs2 = {}
-        
-        
-
-        # self.objs2 = {}
-        # for i in self.objs:
-        #         text = self.font.render(str(i),True,(0,255,0),(0,0,128))
-        #         #textRect = pygame.draw.rect(self.screen,(255,0,0),(self.x,5,50,50))
-        #         #self.screen.blit(text,(self.x,50))
-        #         self.objs2.update({text:(self.x,50)})
-        #         #print("inserted")
-        #         self.x += 150
-
+        self.objs3 = {}
+        if len(self.objs) <= 5:
+            for i, k in self.objPercentage.items():
+                length = k * self.WIDTH
+                self.objs3.update({length:self.HEIGHT})
+  
     #main gameloop
     def run(self):
         self.algo()
         while(True):
+            l = 0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill((0,0,0))
-            text = pygame.draw.rect(self.screen,(255,0,0),(self.x,0,self.W,self.L))
-            text2 = pygame.draw.rect(self.screen,(200,0,0),(self.x2,0,self.L2,self.W2))
-            #self.screen.blit(self.text,(0,0))
-            # for i,j in self.objs2.items():
-            #     self.screen.blit(i,j)
+
+            #renders through a bunch of force brute to get heat maps on the window
+            tt= 0
+            temp = 0
+            for i,j in self.objs3.items():
+                pygame.draw.rect(self.screen,(255,tt+10,0),(self.x+temp,0,i,j))
+                temp += i
+                tt+=50
             pygame.display.flip()
 
 if __name__ == "__main__":
